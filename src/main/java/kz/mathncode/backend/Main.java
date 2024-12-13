c7550db76846783a7cb70ccbfd8cc0539f4af169
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2024-12-13 18:28:41 +0500

    Practical Task

--- src/main/java/kz/mathncode/backend/Main.java
+++ src/main/java/kz/mathncode/backend/Main.java
@@ -2,17 +2,18 @@ package kz.mathncode.backend;
 
 import com.github.javafaker.Faker;
 import kz.mathncode.backend.dao.UserDAO;
-import kz.mathncode.backend.entity.User;
 import kz.mathncode.backend.entity.factory.faker.UserFakerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         //JDBC -- Java Database Connectivity
+        // url-shortener - название базы данных
+
+        //Создать 150 пользователей
         String url = "jdbc:postgresql://localhost:5432/url-shortener?sslmode=disable";
         Connection connection = DriverManager.getConnection(url, "postgres", "admin");
 
@@ -21,11 +22,5 @@ public class Main {
 
         dao.createTableIfNotExists();
         dao.create(factory.produce());
-
-        var readUser = dao.readById(UUID.fromString("9b902fed-19dc-4f78-8729-b7584577fecd"));
-        readUser.setEmail("changed@gmail.com");
-        dao.update(readUser, readUser.getId());
-
-        dao.deleteById(UUID.fromString("e892d011-e418-41d6-a54c-4f958e344ea4"));
     }
 }
