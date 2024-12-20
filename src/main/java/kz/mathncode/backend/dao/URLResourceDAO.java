d7dcf5e6c5b8210c2afb1e2e6255baa016ec42f4
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2024-12-20 18:21:25 +0500

    lesson 2

--- src/main/java/kz/mathncode/backend/dao/URLResourceDAO.java
+++ src/main/java/kz/mathncode/backend/dao/URLResourceDAO.java
@@ -1,4 +1,38 @@
 package kz.mathncode.backend.dao;
 
-public class URLResourceDAO {
+import kz.mathncode.backend.entity.URLResource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class URLResourceDAO extends AbstractDAO<URLResource, UUID> {
+    public URLResourceDAO(Connection connection, Class<URLResource> entityClass, Class<UUID> uuidClass) {
+        super(connection, entityClass, uuidClass);
+    }
+
+    @Override
+    public void create(URLResource entity) throws SQLException {
+
+    }
+
+    @Override
+    public URLResource readById(UUID uuid) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(URLResource entity, UUID uuid) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteById(UUID uuid) throws SQLException {
+
+    }
+
+    @Override
+    public void createTableIfNotExists() throws SQLException {
+
+    }
 }
